8eb68269dac9683ec863664708bade37d58baef8
Yichen Yan <oraluben@outlook.com>
2022-05-10 16:38:06 +0800

    cleanup

--- tests/test_cds_extension/test_flags.py
+++ tests/test_cds_extension/test_flags.py
@@ -5,18 +5,6 @@ from tests import CdsTestMixin
 
 class TestFlags(CdsTestMixin, unittest.TestCase):
     def test_set_mode(self):
-        # valid_mode = [1, -1]
-        # invalid_mode = [i for i in range(-1, 10) if i not in valid_mode]
-        #
-        # for i in valid_mode:
-        #     self.assert_python_source_ok(f'import _cds;'
-        #                                  f'assert _cds.flags.mode == 0;'
-        #                                  f'_cds._set_mode({i});'
-        #                                  f'assert _cds.flags.mode == {i}', PYCDSMODE='MANUALLY')
-        #
-        # for i in invalid_mode:
-        #     self.assert_python_source_failure(f'import _cds; _cds._set_mode({i})')
-
         valid_mode = [2]
         valid_mode_cannot_be_set = [i for i in range(-1, 3) if i not in valid_mode]
         invalid_mode = [i for i in range(-1, 10) if i not in valid_mode and i not in valid_mode_cannot_be_set]
