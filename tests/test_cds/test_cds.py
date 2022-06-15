30c2131ff948756f7580434df15bdee743d08c9c
Yichen Yan <wenji.yyc@alibaba-inc.com>
2022-06-15 09:47:34 +0800

    Add deopt test

--- tests/test_cds/test_cds.py
+++ tests/test_cds/test_cds.py
@@ -1,3 +1,5 @@
+import re
+import sys
 import unittest
 
 from tests import CdsTestMixin
@@ -6,7 +8,7 @@ from . import CDSCase
 
 
 class CDSTest(CdsTestMixin, unittest.TestCase):
-    def test_basic_cds(self):
+    def test_basic_cds1(self):
         with CDSCase(self, self.NAME_LIST, self.TEST_ARCHIVE) as cds:
             cds.run_trace('import enum')
             cds.run_dump()
@@ -32,3 +34,34 @@ class CDSTest(CdsTestMixin, unittest.TestCase):
             cds.run_dump()
             cds.verify_files()
             cds.run_share('import collections')
+
+    @unittest.skipUnless(sys.version_info.minor >= 11, 'test adaptive interpreter since 11')
+    def test_deopt_caches(self):
+        """
+        This test intends to test if all CACHE opcode is de-optimized correctly (aka. set oparg to 0).
+        However, it seems the python module `dis` always return 0 for CACHE ops.
+        So this test is not working as expected now, leave it here for future.
+        """
+
+        with open(self.NAME_LIST, 'w') as f:
+            print('collections', file=f)
+
+        def check_deopt_verifier(_):
+            out = self.assert_python_source_ok(
+                f'import _cds;'
+                f'_cds._load_archive("{self.TEST_ARCHIVE}");'
+                f'd = _cds._get_obj();'
+                f'import dis;'
+                f'[dis.dis(i[1][3], show_caches=True) for i in d]', PYCDSMODE='MANUALLY')
+
+            dis_out: str = out.out.decode()
+            dis_lines = [i for i in dis_out.split('\n') if i and 'CACHE' in i]
+            for i in dis_lines:
+                m = re.search(r'CACHE\s+0', i)
+                self.assertIsNotNone(m, i)
+
+        with CDSCase(self, self.NAME_LIST, self.TEST_ARCHIVE, clear_list=False) as cds:
+            cds.run_dump()
+            cds.verify_files()
+
+            cds.verify(check_deopt_verifier)
